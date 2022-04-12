e45755f802bd8ead7345759d75a1ab527d9078b3
Samiksha Palande <samikshapalande0@gmail.com>
2022-04-12 14:16:19 +0530

    commit 4 - Saved the page file

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,11 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { home , receipt , wallet , person  } from 'ionicons/icons';
+import Tab1 from './pages/tab1Home';
+import Tab2 from './pages/tab2Transaction';
+import Tab3 from './pages/tab3Wallet';
+import Tab4 from './pages/tab4Account';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,31 +42,38 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
+          <Route exact path="/tab1Home">
             <Tab1 />
           </Route>
-          <Route exact path="/tab2">
+          <Route exact path="/tab2Transaction">
             <Tab2 />
           </Route>
-          <Route path="/tab3">
+          <Route path="/tab3Wallet">
             <Tab3 />
           </Route>
+          <Route path="/tab4Account">
+            <Tab4 />
+          </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/tab1Home" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="tab1Home" href="/tab1Home">
+            <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab="tab2Transaction" href="/tab2Transaction">
+            <IonIcon icon={receipt} />
+            <IonLabel>Transaction</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="tab3Wallet" href="/tab3Wallet">
+            <IonIcon icon={wallet} />
+            <IonLabel>Wallet</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4Account" href="/tab4Account">
+            <IonIcon icon={person} />
+            <IonLabel>Account</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
