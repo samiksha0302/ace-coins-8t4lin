76babaaad37cb0a69a2cf68f2b1fbd768f3b3fa3
Samiksha Palande <samikshapalande0@gmail.com>
2022-04-26 00:54:57 +0530

    commit 10 - Login Code Added

--- src/pages/pageLogin.tsx
+++ src/pages/pageLogin.tsx
@@ -1,12 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
     IonLabel, IonButton, IonIcon, IonButtons ,
     IonRow, IonCol, IonGrid, IonItem, 
-    IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonList , IonBackButton, IonListHeader, IonAvatar,IonInput,
-    IonRouterLink
-  
+    IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, 
+    IonList , IonBackButton, IonListHeader, IonAvatar,IonInput,
+    IonRouterLink,
+    IonToast
   } from '@ionic/react';
-  
+
 import './pageLogin.css';
+import { RouteComponentProps } from 'react-router';
+import React from 'react';
 
 import { arrowBack, shapesOutline } from "ionicons/icons";
 import { useEffect, useState } from 'react';
@@ -14,9 +17,106 @@ import { useParams } from 'react-router';
 
 import { home , receipt , wallet , planet, notifications, trendingUp, trendingDown, person  } from 'ionicons/icons';
 import PageSignup from './pageSignup';
-import { monitorEventLoopDelay } from 'perf_hooks';
+import { CONFIG } from '../constnats';
 
-const PageLogin = () => {
+type Props = { props:any };
+type State = { email: string, password: string, toastState: boolean, toastMessage: string};
+
+class PageLogin extends React.Component <Props & RouteComponentProps<any>, State> {
+
+    constructor(props: any) {
+  
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            toastState: false,
+            toastMessage: 'Message'
+        };           
+
+        this.event = new CustomEvent('loggedIn', {detail: false});     
+    }
+
+    event: Event;
+
+    //Code to Update the Input Components with the values.
+    updatePassword = (event: any) => {
+        this.setState({ password: event.detail.value });
+    };
+    
+    updateEmail = (event: any) => {
+        this.setState({ email: event.detail.value });
+    };
+
+    //Code for Clearing Data Before Login
+    componentDidMount(){
+        this.clearCredentials();
+        this.props.history.listen((location, action) => {
+        if(location.pathname === "/login"){
+          this.clearCredentials();}
+        })
+    }
+
+    clearCredentials(){
+        this.event = new CustomEvent('loggedIn', {
+          detail: false
+        });
+        window.dispatchEvent(this.event);   
+        localStorage.removeItem("token");       
+        localStorage.removeItem("username");
+        localStorage.removeItem("isLogin");
+        localStorage.removeItem("email");
+    }
+
+    //Code for Login Check
+    
+    login= () => {
+        
+        let url , credentials;     
+        
+        url = CONFIG.API_ENDPOINT + '/users/login';
+        
+        credentials = {
+            "user": {
+            "email": this.state.email,
+            "password": this.state.password}
+        }
+          
+        fetch(url, {
+                method: 'POST',
+                headers: {"Content-Type": "application/json",},
+                body: JSON.stringify(credentials)})
+                
+                .then((res) => {
+                    
+                    if(res.status === 200){
+                        return res.json();
+                    } else { 
+                        throw new Error("Error Logging in");              
+                    }
+                })
+
+                .then((result) => {
+                     
+                    localStorage.setItem("token",result.user.token);       
+                    localStorage.setItem("username", result.user.username);
+                    localStorage.setItem("isLogin", "true");
+                    localStorage.setItem("email", result.user.email);
+                 
+                    this.event = new CustomEvent('loggedIn', {detail: true,});
+                
+                    window.dispatchEvent(this.event);
+                    this.props.history.replace('/');
+                },
+        
+                (error) => {
+                    console.error(error);           
+                    this.setState({toastMessage: error.toString(), toastState: true});
+                })
+    }
+
+    render() {
 
 	return (
 		<IonPage>
@@ -32,7 +132,7 @@ const PageLogin = () => {
 
                     <IonButtons slot="end">
                         <IonButton>
-                        <IonIcon icon={notifications} />
+                            <IonIcon icon={notifications} />
                         </IonButton>
                     </IonButtons>
                 </IonToolbar>
@@ -42,10 +142,7 @@ const PageLogin = () => {
 
                 <IonRow className='ion-padding'>
                     <IonCol className='ion-text-center ion-padding'>
-                        <IonIcon
-                        style={{ fontSize: "70px", color: "#0040ff" }}
-                        icon={person}
-                        />
+                        <IonIcon style={{ fontSize: "50px", color: "#0040ff" }} icon={person}/>
                     </IonCol>
                 </IonRow>
 
@@ -53,22 +150,20 @@ const PageLogin = () => {
                     <IonCol>
                         <IonItem>
                             <IonLabel position="floating"> Email</IonLabel>
-                            <IonInput  type="email"></IonInput>
+                            <IonInput  onIonChange={this.updateEmail} type="email" placeholder="Email"></IonInput>
                         </IonItem>
                         <IonItem>
                             <IonLabel position="floating"> Password</IonLabel>
-                            <IonInput  type="password"></IonInput>
+                            <IonInput onIonChange={this.updatePassword} type="password" placeholder="Password"></IonInput>
                         </IonItem>
                     </IonCol>
                 </IonRow>
 
                 <IonRow className='ion-padding'>
                     <IonCol>
-                            <IonRouterLink routerLink='./'>
-                                <IonCol size="11">
-                                    <IonButton expand="block">Login</IonButton> 
-                                </IonCol>
-                            </IonRouterLink>
+                        <IonItem>
+                            <IonButton expand="block" onClick={this.login}>Login</IonButton> 
+                        </IonItem>
                     </IonCol>
                     
                     <IonCol>
@@ -80,9 +175,16 @@ const PageLogin = () => {
                     </IonCol>
                 </IonRow>
 
+                <IonToast isOpen={this.state.toastState}
+                    onDidDismiss={() => this.setState(() => ({ toastState: false }))}
+                    message= {this.state.toastMessage}
+                    duration={400}/>
+
             </IonContent>
 		</IonPage>
 	);
+
+}
 };
 
 export default PageLogin;
